8452168f7215c7f7e8e9c4995b016b38b2aadbf3
Shahid-Siddiqui-Raw <136562373+Shahid-Siddiqui-Raw@users.noreply.github.com>
2023-06-14 16:05:05 +0530

    Update Contents.swift

--- day2/MemorySafety.playground/Contents.swift
+++ day2/MemorySafety.playground/Contents.swift
@@ -11,4 +11,13 @@ func increment(_ number: inout Int) {
 
 
 increment(&stepSize)
+/*
+a conflict occurs if you have two accesses that meet all of the following conditions:
+
+At least one is a write access or a nonatomic access.
+
+They access the same location in memory.
+
+Their durations overlap.
+*/
 
